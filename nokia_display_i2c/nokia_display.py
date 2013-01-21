0d2f45942a705a70e8626b486d883e18723a9991
richard Ulrich <richi@paraeasy.ch>
2013-01-21 23:49:54 +0000

    something reaches the display, but trying to submit text freezes the atmega.

--- nokia_display_i2c/nokia_display.py
+++ nokia_display_i2c/nokia_display.py
@@ -20,30 +20,30 @@ class NokiaDisplay:
 
 	def SetContrast(self, val): # adjust the contrast. max is 0x7F
 		self.i2c.write_byte(self.i2cSlaveAddr, 0xB2)
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(val))
+		self.i2c.write_byte(self.i2cSlaveAddr, val)
 
 	def TextOut(self, xpos, ypos, text, big = False): # write text at a given position
 		self.i2c.write_byte(self.i2cSlaveAddr, 0xB3)
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(xpos))
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(ypos))
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(big == True ? 1 : 0))
+		self.i2c.write_byte(self.i2cSlaveAddr, xpos)
+		self.i2c.write_byte(self.i2cSlaveAddr, ypos)
+		self.i2c.write_byte(self.i2cSlaveAddr, 1 if big == True else 0)
 		self.i2c.write_byte(self.i2cSlaveAddr, len(text))
 		for i in range(len(text)):
 			self.i2c.write_byte(self.i2cSlaveAddr, ord(text[i]))
 
 	def SetPixel(self, xpos, ypos, val = 1): # set a single pixel : 0:white  1:black  2:xor
 		self.i2c.write_byte(self.i2cSlaveAddr, 0xB4)
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(xpos))
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(ypos))
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(val))
+		self.i2c.write_byte(self.i2cSlaveAddr, xpos)
+		self.i2c.write_byte(self.i2cSlaveAddr, ypos)
+		self.i2c.write_byte(self.i2cSlaveAddr, val)
 
-	def SetPixel(self, x1, y1, x2, y2, val = 1): # set a single pixel : 0:white  1:black  2:xor
+	def LineOut(self, x1, y1, x2, y2, val = 1): # set a single pixel : 0:white  1:black  2:xor
 		self.i2c.write_byte(self.i2cSlaveAddr, 0xB5)
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(x1))
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(y1))
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(x2))
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(y2))
-		self.i2c.write_byte(self.i2cSlaveAddr, ord(val))
+		self.i2c.write_byte(self.i2cSlaveAddr, x1)
+		self.i2c.write_byte(self.i2cSlaveAddr, y1)
+		self.i2c.write_byte(self.i2cSlaveAddr, x2)
+		self.i2c.write_byte(self.i2cSlaveAddr, y2)
+		self.i2c.write_byte(self.i2cSlaveAddr, val)
 
 	def __repr__(self):
 		print "atmega interfacing a nokia display at i2c address %d" % self.i2cSlaveAddr
@@ -52,8 +52,11 @@ class NokiaDisplay:
 
 # Main program
 if __name__ == "__main__":
-	disp = rf433mhz(0x19, 1) # bus is 0 on the alix, and 1 on the raspberrypi
+	disp = NokiaDisplay(0x13, 1) # bus is 0 on the alix, and 1 on the raspberrypi
+	disp.ClearDisplay()
+	disp.LineOut(5, 5, 70, 20, 1)
 	disp.TextOut(5, 5, "Hello World")
+	disp.UpdateDisplay()
 	
 
 
