a6905ee2a384a67a21316565cf92ace0e221a5f7
Richard Ulrich <richi@paraeasy.ch>
2013-02-04 16:40:52 +0100

    added the sprint planning meeting

--- meetings.py
+++ meetings.py
@@ -31,8 +31,9 @@ def main(config, opts):
         # add the meeting times here
 #        add_meeting(conn, '2012-12-11', 10.00, 11.50)
 #        add_meeting(conn, '2012-12-13', 09.00, 09.50)
-        add_meeting(conn, '2013-01-23', 10.20, 12.25) # scrum 
-        add_meeting(conn, '2013-01-23', 12.75, 14.00) # scrum 
+#        add_meeting(conn, '2013-01-23', 10.20, 12.25) # scrum 
+#        add_meeting(conn, '2013-01-23', 12.75, 14.00) # scrum 
+	add_meeting(conn, '2013-02-04', 15.00, 16.50)  
 
 	conn.close()
 
