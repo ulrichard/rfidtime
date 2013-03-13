40015cb8842103a9a1dff5c8b52c5c0b24215319
Richard Ulrich <richi@paraeasy.ch>
2013-03-13 23:01:15 +0100

    trying with a simpler htlm only lib, as that should be enough. But still no luck

--- bamboo.py
+++ bamboo.py
@@ -5,30 +5,31 @@
 # https://dev.cubx-software.com:8446/rest/api/latest/result/PLB-CIMAINDEV?max-results=5
 
 
-from restkit import request, Resource, BasicAuth
+import httplib2, getpass
 
-class BambooRest(Resource):
-	def __init__(self, url, user, password, **kwargs):
+class BambooState:
+	def __init__(self, url, user, password):
 		self.baseurl = url
-		self.auth = BasicAuth(user, password)
-		Resource.__init__(self, self.baseurl, follow_redirect=True, max_follow_redirect=10, filters=[self.auth], **kwargs)
-
-	def request(self, *args, **kwargs):
-		resp = Resource.request(self, *args, **kwargs)
-		data_body = resp.body_string()
+		self.user = user
+		self.passwd = password
 
 	def latestBuildSuccessful(self):
-		
-		req = self.get('/rest/api/latest/result/PLB-CIMAINDEV?max-result=5&os_authType=basic')
-		req.body_string()
-		
+		h = httplib2.Http(".cache")
+		h.debuglevel = 2
+		h.disable_ssl_certificate_validation = True
+		h.add_credentials(self.user, self.passwd)
+#		url = "https://%s:%s@%s/rest/api/latest/result/PLB-CIMAINDEV?max-result=5&os_authType=basic" % (self.user, self.passwd, self.baseurl)
+		url = "https://%s/rest/api/latest/result/PLB-CIMAINDEV?max-result=5&os_authType=basic" % self.baseurl
+#		resp, content = h.request(url, "GET", headers={'content-type':'text/json'} )
+		resp, content = h.request(url, "GET")
+		print content	
 		return True
 
 
 	
 # test code
 if __name__ == "__main__":
-	passwd = raw_input("Enter the password for ulr: ")
-	bamb = BambooRest('https://dev.cubx-software.com:8446', 'ulr', passwd)
+	passwd = getpass.getpass(prompt="Enter the password for ulr: ")
+	bamb = BambooState('dev.cubx-software.com:8446', 'ulr', passwd)
 	print bamb.latestBuildSuccessful()
 
