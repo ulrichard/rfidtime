10d7c557f35551aefa6c11b6780a6aac90f62a36
Richard Ulrich <richi@paraeasy.ch>
2013-04-27 00:55:09 +0200

    added an LED and verified that i2c works

--- hitachi_display/hitachi_display.ino
+++ hitachi_display/hitachi_display.ino
@@ -1,54 +1,49 @@
-/*
-  LiquidCrystal Library - Hello World
+// Hitachi HD44780 display
+// For the circuitry, see the acompanyoning fritzing file.
  
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the 
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD
- and shows the time.
- 
-  The circuit:
- * LCD RS pin to digital pin 8 
- * LCD Enable pin to digital pin 7
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
- 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- 
- This example code is in the public domain.
 
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
-
-// include the library code:
+// Arduino
 #include <LiquidCrystal.h>
+#include <Wire.h>
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 7, 5, 4, 3, 2);
+const uint8_t LED_RED = 9;
 
-void setup() {
-  // set up the LCD's number of columns and rows: 
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+uint8_t recvBuffer[40];
+uint8_t recvPos;
+unsigned long recvLast;
+
+void setup()
+{
+	Wire.begin(0x21); // join i2c bus with address #0x21
+    Wire.onReceive(receiveI2C); // register event
+
+	// set up the LCD's number of columns and rows: 
+	lcd.begin(16, 2);
+	// Print a message to the LCD.
+	lcd.print("hello, world!");
+
+	pinMode(LED_RED, OUTPUT);
+	digitalWrite(LED_RED, HIGH);
+	delay(1000);
+    digitalWrite(LED_RED, LOW);
 }
 
-void loop() {
+void receiveI2C(int howMany)
+{
+    for(int i=0; i<howMany; ++i)
+    {
+        const uint8_t val = Wire.read();
+        
+        if(recvPos + 1 < sizeof(recvBuffer))
+            recvBuffer[recvPos++] = val;
+    }
+    recvLast = millis();
+}
+
+void loop()
+{
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
