c3771247cd29e73949b5228945b1c21e828d1473
Richard Ulrich <richi@paraeasy.ch>
2014-01-27 00:27:31 +0100

    started with a pure python lib that begrudingly doesn't work out of the box and is hardwired for the reaspberrypi

--- nfc_shield_pn532.py
+++ nfc_shield_pn532.py
@@ -4,20 +4,29 @@
 # https://www.adafruit.com/products/364
 # http://nfc-tools.org/index.php?title=Libnfc#Debian_.2F_Ubuntu
 # http://packages.debian.org/source/sid/libnfc
+# http://hubcitylabs.org/nfc-on-raspberrypi-with-pn532-py532lib-and-i2c/
 
+import smbus, time, math, os, sys
+from rfid532lib.py532lib.i2c import *
+from rfid532lib.py532lib.frame import *
+from rfid532lib.py532lib.constants import *
 
-import smbus, time, math
 
 class NfcShield:
 	def __init__(self, i2cSlaveAddr = 0x24, i2cBusNbr = 0):
-		self.i2cSlaveAddr = i2cSlaveAddr
-		self.i2c = smbus.SMBus(i2cBusNbr)
+#		self.i2cSlaveAddr = i2cSlaveAddr
+#		self.i2c = smbus.SMBus(i2cBusNbr)
+		self.pn532 = Pn532_i2c()
+		self.pn532.SAMconfigure()
 
-	def GetFirmwareVersion(self): # Checks the firmware version of the PN5xx chip
+#	def GetFirmwareVersion(self): # Checks the firmware version of the PN5xx chip
 #		self.i2c.write_byte(self.i2cSlaveAddr, 0x02) # PN532_COMMAND_GETFIRMWAREVERSION
 #		time.sleep(0.002) 
-		data = self.i2c.read_i2c_block_data(self.i2cSlaveAddr, 0x02)
-		return data
+#		data = self.i2c.read_i2c_block_data(self.i2cSlaveAddr, 0x02)
+#		return data
+
+	def GetCardData():
+		return self.pn532.read_mifare().get_data()
 
 	def __repr__(self):
 		print "This is a library for the Adafruit PN532 NFC/RFID shield"
@@ -27,6 +36,7 @@ class NfcShield:
 # test code
 if __name__ == "__main__":
 	nfc = NfcShield(0x24, 1) # bus is 0 on the alix, and 1 on the raspberryPi
-	vers = nfc.GetFirmwareVersion()
-	print vers
+#	print nfc.GetFirmwareVersion()
+
+	print nfc.GetCardData()
 
