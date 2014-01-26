964250f883e8f4fe59118686a9b8b9eb617ecef2
Richard Ulrich <richi@paraeasy.ch>
2014-01-27 00:47:05 +0100

    switching this part to python3 and get it to work so far

--- nfc_shield_pn532.py
+++ nfc_shield_pn532.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # script to communicate with the nfc shie 
 # http://www.elecfreaks.com/store/rfidnfc-breakout-board-p-519.html
 # https://www.adafruit.com/products/364
@@ -6,7 +6,11 @@
 # http://packages.debian.org/source/sid/libnfc
 # http://hubcitylabs.org/nfc-on-raspberrypi-with-pn532-py532lib-and-i2c/
 
-import smbus, time, math, os, sys
+import time, math, os, sys
+
+lib_path = os.path.abspath('.')
+sys.path.append(lib_path + '/rfid532lib')
+
 from rfid532lib.py532lib.i2c import *
 from rfid532lib.py532lib.frame import *
 from rfid532lib.py532lib.constants import *
@@ -25,18 +29,17 @@ class NfcShield:
 #		data = self.i2c.read_i2c_block_data(self.i2cSlaveAddr, 0x02)
 #		return data
 
-	def GetCardData():
+	def GetCardData(self):
 		return self.pn532.read_mifare().get_data()
 
-	def __repr__(self):
-		print "This is a library for the Adafruit PN532 NFC/RFID shield"
-
+#	def __repr__(self):
+		print('This is a library for the Adafruit PN532 NFC shield')
 
 
 # test code
-if __name__ == "__main__":
+if __name__ == '__main__':
 	nfc = NfcShield(0x24, 1) # bus is 0 on the alix, and 1 on the raspberryPi
 #	print nfc.GetFirmwareVersion()
 
-	print nfc.GetCardData()
+	print(nfc.GetCardData())
 
